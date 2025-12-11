1e2855a605d1b30bb641dfc57c5fb9f78d88a926
Maxim Funk <75086660+Mugen1992@users.noreply.github.com>
2025-12-12 01:16:44 +0500

    Add initial xUnit test project

---
+++ MaxFunkTetris2024.Tests/SmokeTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace MaxFunkTetris2024.Tests
+{
+    public class SmokeTests
+    {
+        [Fact]
+        public void True_is_true()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+// Разрешаем тестовому проекту видеть internal-типы основной сборки
+[assembly: InternalsVisibleTo("MaxFunkTetris2024.Tests")]
+
