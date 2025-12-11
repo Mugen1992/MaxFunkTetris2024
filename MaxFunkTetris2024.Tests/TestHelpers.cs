a6db157aa6ce9c9d9367705598d05a4068bd4c4c
Maxim Funk <75086660+Mugen1992@users.noreply.github.com>
2025-12-12 02:00:51 +0500

    Исправить изоляцию тестов и доступность вспомогательных методов (№20)

--- MaxFunkTetris2024.Tests/TestHelpers.cs
+++ MaxFunkTetris2024.Tests/TestHelpers.cs
@@ -2,28 +2,28 @@ using MaxFunkTetris2024;
 
 namespace MaxFunkTetris2024.Tests
 {
-    public static class TestHelpers
+    internal static class TestHelpers
     {
         // Фабрика для создания игрового поля с заданными размерами
-        public static Program.GameBoard CreateBoard(int width, int height)
+        internal static Program.GameBoard CreateBoard(int width, int height)
         {
             return new Program.GameBoard(width, height);
         }
 
         // Стандартная доска 10x20 для большинства тестов
-        public static Program.GameBoard CreateDefaultBoard()
+        internal static Program.GameBoard CreateDefaultBoard()
         {
             return CreateBoard(10, 20);
         }
 
         // Фабрика для получения тетрамино по индексу формы
-        public static Program.Tetromino CreateTetromino(int shapeIndex)
+        internal static Program.Tetromino CreateTetromino(int shapeIndex)
         {
             return new Program.Tetromino(shapeIndex);
         }
 
         // Утилита для поклеточного сравнения двумерных массивов формы
-        public static bool AreShapesEqual(int[,] left, int[,] right)
+        internal static bool AreShapesEqual(int[,] left, int[,] right)
         {
             if (left.GetLength(0) != right.GetLength(0) || left.GetLength(1) != right.GetLength(1))
             {
